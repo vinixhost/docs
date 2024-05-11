9f2f42f2b5fa044196c40951bbbfb7df2e887df5
alieldeba <alieldeba375@gmail.com>
2024-05-11 13:11:01 +0300

    build: project with nextra

---
+++ theme.config.jsx
@@ -0,0 +1,17 @@
+export default {
+  logo: <strong>VinixHost</strong>,
+  project: {
+    link: "https://github.com/vinixhost/docs",
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s | Vinix Host",
+    };
+  },
+  footer: {
+    text: "Copyright Vinix Host 2024",
+  },
+  chat: {
+    link: "https://discord.gg/STz6dMhX",
+  },
+};
